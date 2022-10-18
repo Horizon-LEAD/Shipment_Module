b6c591299db365c112c8b84992beebb3b3feedc9
Andreas Alexopoulos <andalexo@gmail.com>
2022-10-18 18:50:35 +0300

    updates envctl to be in line with the other repos

--- src/shipmentsynth/envctl.py
+++ src/shipmentsynth/envctl.py
@@ -8,12 +8,13 @@ from json import loads, JSONDecodeError
 BOOL_VALUES = ('true', 't', 'on', '1', 'false', 'f', 'off', '0', "")
 BOOL_TRUE_VALUES = ('true', 't', 'on', '1')
 
-PARAMS_BOOL = ["NEAREST_DC"]
-PARAMS_NUM = ["YEARFACTOR", "PARCELS_GROWTHFREIGHT"]
-PARAMS_STR = ["LABEL", "SHIPMENTS_REF", 
+PARAMS_STR = ["LABEL", "SHIPMENTS_REF",
               "FAC_LS0", "FAC_LS1", "FAC_LS2", "FAC_LS3",
               "FAC_LS4", "FAC_LS5", "FAC_LS6", "FAC_LS7"]
+PARAMS_BOOL = ["NEAREST_DC"]
+PARAMS_NUM = ["YEARFACTOR", "PARCELS_GROWTHFREIGHT"]
 PARAMS_LIST_STR = []
+PARAMS_LIST_BOOL = []
 PARAMS_LIST_NUM = []
 PARAMS_JSON = []
 
@@ -55,18 +56,14 @@ def parse_env_values(env):
             config_env[key] = to_bool(env[key])
         for key in PARAMS_NUM:
             config_env[key] = float(env[key])
-        for key in PARAMS_JSON:
-            config_env[key] = loads(env[key])
         for key in PARAMS_LIST_STR:
-            if env[key] == '':
-                config_env[key] = []
-            else:
-                config_env[key] = env[key].split(',')
+            config_env[key] = [] if env[key] == '' else env[key].split(',')
+        for key in PARAMS_LIST_BOOL:
+            config_env[key] = [] if env[key] == '' else list(map(to_bool, env[key].split(',')))
         for key in PARAMS_LIST_NUM:
-            if env[key] == '':
-                config_env[key] = []
-            else:
-                config_env[key] = list(map(float, env[key].split(',')))
+            config_env[key] = [] if env[key] == '' else list(map(float, env[key].split(',')))
+        for key in PARAMS_JSON:
+            config_env[key] = {} if env[key] == '' else loads(env[key])
     except KeyError as exc:
         raise KeyError("Failed while parsing environment configuration") from exc
     except JSONDecodeError as exc:
